c127fa030a940d0b253c989d41cf2aa416a0bcf2
mishraom12345 <mishrahariom525@gmail.com>
2023-02-05 16:40:16 +0530

    Adding cart button

--- src/Componet/Layout/Header.js
+++ src/Componet/Layout/Header.js
@@ -1,7 +1,7 @@
 import { Fragment } from "react"
 import mealsimage from '../../asset/meals.jpg.png'
 import classes from './Header.module.css'
-
+import HeaderCartButton from "./HeaderCartButton"
 
 const Header = props=>{
     return(
@@ -9,7 +9,7 @@ const Header = props=>{
             <header className = {classes.header}>
 
                 <h1>Indian Meals</h1>
-                <button>Cart</button>
+                <HeaderCartButton />
             </header>
             <div className={classes['main-image']}>
                 <img src = {mealsimage} alt = 'a table full of indian food ' />
