453ee60bb0890ee350dcb3ce9dce84d346996545
mishraom12345 <mishrahariom525@gmail.com>
2023-02-06 18:39:03 +0530

    Making a hard Coded Cart without any Functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import Cart from "./Componet/Cart/Cart";
 import Header from "./Componet/Layout/Header";
+import Meal from "./Componet/Meal/Meal";
+
 function App() {
   return (
     <div>
+      <Cart />
       <Header />
+      <main>
+        <Meal />
+      </main>
     </div>
   );
 }
--- src/Componet/Layout/Header.js
+++ src/Componet/Layout/Header.js
@@ -8,7 +8,7 @@ const Header = props=>{
         <Fragment  >
             <header className = {classes.header}>
 
-                <h1>Indian Meals</h1>
+                <h1>Indian  Meals</h1>
                 <HeaderCartButton />
             </header>
             <div className={classes['main-image']}>
